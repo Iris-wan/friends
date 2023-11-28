5a544086c16e674b9069b5917741782767217d27
Sirui <s774wang@uwaterloo.ca>
2023-11-28 14:16:19 -0500

    accessibility fixed

--- js/character.js
+++ js/character.js
@@ -6,7 +6,7 @@ menuBars.addEventListener('click', function() {
     menuBars.classList.toggle('change');
     navList.classList.toggle('change');
     nav.classList.toggle('change');
-    var main = document.getElementById('main');
+    var main = document.querySelector('.main');
     var footer = document.getElementById('footer');
     if(main.style.display !== 'none') {
         main.style.display = 'none';
